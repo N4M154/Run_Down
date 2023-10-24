08589b889a645aeff3d077c4814156215b5e2bd1
Faiza Maliat <122308040+FaizaM07@users.noreply.github.com>
2023-10-24 10:27:21 +0600

    prediction

--- Program.cs
+++ Program.cs
@@ -8,6 +8,16 @@ namespace Toss
 {
     class Program
     {
+
+        static double PredictWinPercentage(double teamStrength1, double teamStrength2, double form1, double form2, double pitchFactor)
+        {
+            double totalStrength = teamStrength1 + teamStrength2;
+            double team1Contribution = (teamStrength1 * form1 + teamStrength2 * form2) / totalStrength;
+            double team2Contribution = (teamStrength1 * form2 + teamStrength2 * form1) / totalStrength;
+            double winningProbability = (team1Contribution + pitchFactor) / (team1Contribution + team2Contribution + pitchFactor);
+            return winningProbability * 100;
+        }
+
         static void Main(string[] args)
         {
 
@@ -53,6 +63,40 @@ namespace Toss
             {
                 Console.WriteLine("Invalid choice. Please choose either 'B' or 'BL'.");
             }
+
+
+
+
+
+
+            Console.Write("Enter the strength of " + team1 + ": ");
+            double teamStrength1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter the strength of " + team2 + ": ");
+            double teamStrength2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter the current form of " + team1 + " (0 to 1): ");
+            double form1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter the current form of " + team2 + " (0 to 1): ");
+            double form2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter pitch conditions factor (0 to 1, 0.5 for neutral): ");
+            double pitchFactor = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Predicted Winning Percentage:");
+            double percentTeam1 = PredictWinPercentage(teamStrength1, teamStrength2, form1, form2, pitchFactor);
+            double percentTeam2 = 100 - percentTeam1;
+
+            Console.WriteLine($"{team1}: {percentTeam1}%");
+            Console.WriteLine($"{team2}: {percentTeam2}%");
+
+
+
+
+
+
         }
     }
 
