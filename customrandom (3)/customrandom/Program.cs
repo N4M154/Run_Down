cf88ffff03ef5eaa8a356a7e103cb3c1e801dbb8
Namisa Najah <127068077+N4N48@users.noreply.github.com>
2023-10-24 10:08:53 +0600

    nabilartar updated version

---
+++ customrandom (3)/customrandom/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace customrandom
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            // Get current timestamp as the seed value
+            int seed = (int)DateTime.Now.Ticks;
+
+            // Initialize custom random number generator with the timestamp as seed
+            Custom customRandom = new Custom(seed);
+
+            // Generate a pseudo-random number
+            int randomNumber = customRandom.Next();
+
+            // Use the pseudo-random number to simulate a toss (0 for Heads, 1 for Tails)
+            int tossResult = randomNumber % 2;
+
+            // Convert the toss result to Heads or Tails
+            string result = (tossResult == 0) ? "Heads" : "Tails";
+
+            // Output the result
+            Console.WriteLine($"Toss result: {result}");
+
+            Console.ReadKey();
+        }
+    }
+}
+class Custom
+{
+    private int seed;
+
+    public Custom(int seed)
+    {
+        this.seed = seed;
+    }
+
+    public int Next()
+    {
+        seed = (seed * 1103515245 + 12345) & 0x7FFFFFFF;
+        return seed;
+    }
+}
