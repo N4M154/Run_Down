56fd37d3605f14d4ee929bdc49007f4c65648460
Faiza Maliat <122308040+FaizaM07@users.noreply.github.com>
2023-10-24 19:06:29 +0600

    Main

--- Program.cs
+++ Program.cs
@@ -9,15 +9,6 @@ namespace Toss
     class Program
     {
 
-        static double PredictWinPercentage(double teamStrength1, double teamStrength2, double form1, double form2, double pitchFactor)
-        {
-            double totalStrength = teamStrength1 + teamStrength2;
-            double team1Contribution = (teamStrength1 * form1 + teamStrength2 * form2) / totalStrength;
-            double team2Contribution = (teamStrength1 * form2 + teamStrength2 * form1) / totalStrength;
-            double winningProbability = (team1Contribution + pitchFactor) / (team1Contribution + team2Contribution + pitchFactor);
-            return winningProbability * 100;
-        }
-
         static void Main(string[] args)
         {
 
