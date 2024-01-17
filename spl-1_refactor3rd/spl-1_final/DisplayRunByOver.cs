2303536b40b8da2655e7bcb6db673cddd0d2895c
Namisa Najah <namisa.najah.raisa@gmail.com>
2024-01-17 17:49:14 +0600

    RunByOver graph

---
+++ spl-1_refactor3rd/spl-1_final/DisplayRunByOver.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace scoreboard
+{
+    public class DisplayRunByOver: IDisplayRunByOver
+    {
+        public void DisplayRunbyOverGraph(int[] runsScored)
+        {
+
+
+            Console.WriteLine("------------------------");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Cricket Run-by-Over Graph");
+            Console.WriteLine("------------------------");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            // Find the maximum number of runs scored in an over
+            int maxRuns = runsScored.Max();
+
+            // Display the run-by-over graph vertically from bottom to top
+            for (int i = maxRuns; i > 0; i--)
+            {
+                for (int j = 0; j < runsScored.Length; j++)
+                {
+                    if (i <= runsScored[j])
+                    {
+                        Console.Write("|");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.Write("  "); // Adjust spacing for better visualization
+                }
+                Console.WriteLine();
+            }
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+
+            // Display the over numbers
+            for (int j = 0; j < runsScored.Count(); j++)
+            {
+                Console.Write($"{j + 1}  ");
+                // Console.Write($"(runsScored)");
+            }
+
+            Console.WriteLine();
+
+            Console.ResetColor();
+            Console.WriteLine("------------------------");
+        }
+    }
+}
